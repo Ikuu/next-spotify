465b253ae1269f0c28b542cfed7c7ae47caca811
Alexander Anderson <alex.anderson@dogdigital.com>
2018-08-23 16:20:24 +0100

    Refactored Index into Component and added statics.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/layouts';
 
-function Index() {
-  return (
-    <Layout>
-      <h1>Content</h1>
-    </Layout>
-  );
+class Index extends PureComponent {
+  static getInitialProps = async () => {
+    const res = await fetch('http://localhost:3023/api/artist/drake');
+    const json = await res.json();
+
+    return {
+      artist: json.items[0],
+    };
+  }
+
+  static propTypes = {
+    artist: {
+      name: PropTypes.string,
+    },
+  };
+
+  static defaultProps = {
+    artist: {},
+  }
+
+  render() {
+    const { artist } = this.props;
+
+    return (
+      <Layout>
+        <h1>Content</h1>
+        <p>
+          {artist.name}
+        </p>
+      </Layout>
+    );
+  }
 }
 
 export default Index;
